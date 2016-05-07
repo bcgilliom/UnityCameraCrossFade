04108fb733ac9ba5fa85a0d1a7c6abbb98bb9736
Brian Cooley-Gilliom <brian@brianmbp.home>
2016-05-07 18:35:34 +0100

    A very simple demo scene for camera cross fade using the Screen Overlay image effect and a render texture.

---
+++ Assets/CameraRTBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class CameraRTBehavior : MonoBehaviour {
+
+	ScreenOverlay screenOverlay;
+
+	RenderTexture renderTexture;
+	Texture2D texture;
+
+	// Use this for initialization
+	void Start () {
+		screenOverlay = Camera.main.GetComponent<ScreenOverlay> ();
+		renderTexture = GetComponent<Camera> ().targetTexture;
+
+		texture = new Texture2D (renderTexture.width, renderTexture.height);
+		screenOverlay.texture = texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RenderTexture.active = renderTexture;
+
+		texture.ReadPixels (new Rect (0, 0, texture.width, texture.height), 0, 0);
+
+		texture.Apply ();
+	}
+}
